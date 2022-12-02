ab1586461dcd807cea45f1bd453b6d3a4b33438c
Manav Misra <manavm@visionwebsoft.com>
2022-12-01 23:57:07 -0600

    feat: load searched 🔍 image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+
+// TODO: Append the search terms (separated by commas) to the URL
+const BASE_URL = "https://source.unsplash.com/random?";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [searchTerms, setSearchTerms] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setSearchTerms(event.target.search.value);
+  };
+
+  return (
+    <>
+      <h1 className="my-8 text-center text-3xl font-black">
+        Display a Random Image
+      </h1>
+
+      <main className="flex flex-col items-center justify-center">
+        <form
+          onSubmit={handleSubmit}
+          className="mb-8 flex flex-col items-center gap-y-4"
+        >
+          <label htmlFor="search">
+            Search (separate terms with commas - no spaces)
+          </label>
+          <input
+            type="search"
+            id="search"
+            className="rounded-sm border px-1"
+            placeholder="🔍"
+          />
+          <button
+            type="submit"
+            className="rounded-full bg-green-500 p-4 text-white"
+          >
+            Go!
+          </button>
+        </form>
+
+        {/* Only show this if there is a current search term  */}
+        {searchTerms && (
+          <img
+            src={`${BASE_URL}${searchTerms}`}
+            alt={`Random ${searchTerms}`}
+          />
+        )}
+      </main>
+    </>
+  );
 }
 
 export default App;
